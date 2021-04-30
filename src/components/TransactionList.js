92fb2c154ee79046a655ecd861594f06822d1c49
thestreaky <jiachaowu@hotmail.com>
2021-04-30 02:16:10 -0400

    practice 1

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,15 +1,16 @@
 import React,{useContext} from 'react'
 import {GlobalContext} from '../context/GlobalState'
+import {Transaction} from './Transaction'
+
 export const TransactionList = () => {
-    const context = useContext(GlobalContext);
-    
+    const {transactions} = useContext(GlobalContext);
+
     return (
         <>
             <h3>History</h3>
             <ul id="list" className="list">
-                <li className="minus"> 
-                    Cash <span>-$400</span> <button className = "delete-btn">X</button>
-                </li>
+                {transactions.map(transaction=>( <Transaction key={transaction.id} transaction={transaction}/>))}
+               
             </ul>            
         </>
     )
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,10 +4,10 @@ import AppReducer from './AppReducer';
 
 const initialState={
     transactions:[
-        {id:1,text:'Flower',amount:-20},
-        {id:2,text:'Salary',amount:300},
-        {id:3,text:'Book',amount:-10},
-        {id:4,text:'Camera',amount:150}
+        // {id:1,text:'Flower',amount:-20},
+        // {id:2,text:'Salary',amount:300},
+        // {id:3,text:'Book',amount:-10},
+        // {id:4,text:'Camera',amount:150}
     ]
 }
 
@@ -17,8 +17,26 @@ export const GlobalContext=createContext(initialState);
 export const GlobalProvider = ({children}) =>{
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    //action 
+
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload:id
+        });
+    }
+
+    function addTransaction(id){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload:id
+        });
+    }
+
     return (<GlobalContext.Provider value={{
-        transactions:state.transactions
+        transactions:state.transactions,
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>
